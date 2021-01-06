1063c8a8b55912a39a0ba1c343a10748376b20a3
Harsh Gautam <hgautam208@gmail.com>
2021-01-07 01:12:01 +0530

    Added basic search funtionality in blog

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from core.views import home, contact, about
+from core.views import home, contact, about, search
 
 urlpatterns = [
     path('', home, name="Home"),
     path('about', about, name="About"),
     path('contact', contact, name="Contact"),
+    path('search', search, name="Search"),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from core.models import ContactForm
-import json
-import time
+from blog.models import Post
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
@@ -23,3 +22,17 @@ def contact(request):
 
 def about(request):
     return render(request, 'core/about.html')
+
+def search(request):
+    query = request.GET['search']
+    if len(query) > 50:
+        allPosts = Post.objects.none()
+    else:
+        posts_title = Post.objects.filter(title__icontains=query)
+        posts_content = Post.objects.filter(content__icontains=query)
+        posts_author = Post.objects.filter(author__icontains=query)
+
+        allPosts = posts_title.union(posts_content).union(posts_author)
+
+    context = {"allPosts": allPosts, "query": query}
+    return render(request, "core/search.html", context)
